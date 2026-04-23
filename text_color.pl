6de2c9993a46de1402944c6c095c544c931adbb1
Scott Baker <scott@perturb.org>
2026-04-23 16:02:55 -0700

    Updated, more functional, `color()` function

--- text_color.pl
+++ text_color.pl
@@ -79,31 +79,29 @@ foreach my $line (@lines) {
 sub color {
 	my ($str, $txt) = @_;
 
-	# If we're NOT connected to a an interactive terminal don't do color
-	if (-t STDOUT == 0) { return $txt // ""; }
+	if (-t STDOUT == 0 || $ENV{NO_COLOR}) { return $txt // ""; } # No interactive terminal
+	if (!length($str) || $str eq 'reset') { return "\e[0m";    } # No string = RESET
 
-	# No string sent in, so we just reset
-	if (!length($str) || $str eq 'reset') { return "\e[0m"; }
-
-	# Some predefined colors
+	# Some predefined colors/commands
 	my %color_map = qw(red 160 blue 27 green 34 yellow 226 orange 214 purple 93 white 15 black 0);
-	$str =~ s|([A-Za-z]+)|$color_map{$1} // $1|eg;
+	my %cmd_map   = qw(bold 1 italic 3 underline 4 blink 5 inverse 7);
 
-	# Get foreground/background and any commands
-	my ($fc,$cmd) = $str =~ /^(\d{1,3})?_?(\w+)?$/g;
-	my ($bc)      = $str =~ /on_(\d{1,3})$/g;
+	# Pre-process the string.
+	$str =~ s/on_/-/;                              # "on_" becomes a negative number
+	$str =~ s|([A-Za-z]+)|$color_map{$1} // $1|eg; # command number
 
-	if (defined($fc) && int($fc) > 255) { $fc = undef; } # above 255 is invalid
+	my @parts = split("_", $str);
+	foreach my $p (@parts) {
+		my $cmd_num = $cmd_map{$p // 0};
 
-	# Some predefined commands
-	my %cmd_map = qw(bold 1 italic 3 underline 4 blink 5 inverse 7);
-	my $cmd_num = $cmd_map{$cmd // 0};
+		if    ($cmd_num)                      { $p = $cmd_num;  }
+		elsif (defined($p) && $p =~ /^-(.+)/) { $p = "48;5;$p"; }
+		elsif (defined($p))                   { $p = "38;5;$p"; }
+	}
 
-	my $ret = '';
-	if ($cmd_num)      { $ret .= "\e[${cmd_num}m"; }
-	if (defined($fc))  { $ret .= "\e[38;5;${fc}m"; }
-	if (defined($bc))  { $ret .= "\e[48;5;${bc}m"; }
-	if (defined($txt)) { $ret .= $txt . "\e[0m";   }
+	my $ret = "\e[" . join(";", @parts) . "m";
+
+	if (defined($txt)) { $ret .= $txt . "\e[0m"; }
 
 	return $ret;
 }
